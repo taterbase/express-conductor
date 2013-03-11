b072e430be2808c3a184e68eda17aadcf489b3a2
taterbase <shankga@gmail.com>
2013-03-10 21:35:15 -0600

    Make mock of Express a little fancier, using get, put, post, and del

--- test/mock/express.js
+++ test/mock/express.js
@@ -4,11 +4,53 @@ function App() {
   if(!(this instanceof App))
     return new App();
 
-  this.routes = {};
+  this.routes = {
+    'get': {}
+  , 'put': {}
+  , 'del': {}
+  , 'post': {}
+  };
+
+
 }
 
 App.prototype.get = function(route, action) {
-  this.routes[route] = action;
+  this.routes.get[route] = action;
+};
+
+App.prototype.put = function(route, action) {
+  this.routes.put[route] = action;
+};
+
+App.prototype.del = function(route, action) {
+  this.routes.del[route] = action;
+};
+
+App.prototype.post = function(route, action) {
+  this.routes.post[route] = action;
+};
+
+/*********
+  Data: {
+    params: Array
+  , query: Object
+  , body: String
+  }
+*********/
+App.prototype.makeRequest = function(route, method, data, cb) {
+  var res = new Res(cb);
+  this.routes[method](req, res, null);
 };
 
 module.exports = App;
+
+function Res(cb) {
+
+  Res.prototype.json = function(data) {
+    cb(null, JSON.stringify(data));
+  };
+
+  return this;
+
+}
+
